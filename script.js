6ea21a1c2a2daba0ccc9c901c33bc16051f66da3
Choigan <chojgan.ondar95@gmail.com>
2023-10-29 23:32:04 +0700

    init: render btns and create screen

---
+++ script.js
@@ -0,0 +1,28 @@
+const btnsBox = document.getElementById('btnsBox');
+const screenBox = document.getElementById('screenBox')
+const currentExpression = document.getElementById('currentExpression');
+const screenBasicValue = document.getElementById('screenBasicValue');
+
+
+const btnList = {};
+const btnValue = ['C', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+const createBtn = (index) => {
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.textContent = btnValue[index];
+    btnsBox.append(btn);
+    btnList['btn' + index] = btn
+}
+
+function renderBtn(count) {
+    for (let index = 0; index < count; index++) {
+        createBtn(index);
+    }
+}
+
+renderBtn(19);
+
+btnList.btn16.onclick = () => {
+    alert('ok')
+}
---
+++ style.js
@@ -0,0 +1,12 @@
+btnList.btn16.style.gridColumn = 'span 2';
+
+btnList.btn3.style.background = 'orange';
+btnList.btn7.style.background = 'orange';
+btnList.btn11.style.background = 'orange';
+btnList.btn15.style.background = 'orange';
+btnList.btn18.style.background = 'orange';
+
+
+btnList.btn0.style.background = 'gray';
+btnList.btn1.style.background = 'gray';
+btnList.btn2.style.background = 'gray';
